2959e409921d2aa703d456d474e56ec36340ce20
Vieux12 <99101703+Vieux12@users.noreply.github.com>
2022-11-12 18:38:18 +0200

    Update Contact.jsx

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './contact.css'
+import {MdOutLineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
 const contact = () => {
